5ff7a39e97ea6d01136e6aea014e04e517573715
Aravindr1986 <ramachandran.aravind@gmail.com>
2020-01-20 05:49:49 -0500

    Very basic node js samples 1.0

---
+++ basic_samples/myfirst.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var dt=require('./myfirstmodule')
+var url =require('url');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Hello World!');
+    res.write("The current date is :"+dt.myDateTime());
+    var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+  }).listen(8080);
